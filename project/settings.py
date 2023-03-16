8c5d2e9ec75ddea789ddd25f2fce0f1cb26368ff
garzo94 <alexgarzo25@gmail.com>
2023-03-16 16:56:10 -0600

    gunicorn

--- project/settings.py
+++ project/settings.py
@@ -89,10 +89,21 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'containers-us-west-202.railway.app',
+        'PORT': '7006',
     }
 }
 
